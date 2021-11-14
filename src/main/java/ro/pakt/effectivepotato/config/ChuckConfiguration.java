1df719957f530dbd15292fb371f9261562d0ef58
Andrei Gheorghiu <gheorghiu.andrei@outlook.com>
2021-11-14 08:24:05 +0200

    move bean config to xml

--- src/main/java/ro/pakt/effectivepotato/config/ChuckConfiguration.java
+++ src/main/java/ro/pakt/effectivepotato/config/ChuckConfiguration.java
@@ -1,14 +1,14 @@
 package ro.pakt.effectivepotato.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 
-@Configuration
+// @Configuration
 public class ChuckConfiguration {
 
-    @Bean
+    // @Bean
     public ChuckNorrisQuotes chuckNorrisQuotes() {
         return new ChuckNorrisQuotes();
     }
